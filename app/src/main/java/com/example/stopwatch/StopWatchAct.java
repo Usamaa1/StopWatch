21f13d60dd08d18b8261369a2d6c2ced09dcdffd
Haider <shkhaider2015@gmail.com>
2020-01-09 18:12:09 +0500

    chronometer complete

--- app/src/main/java/com/example/stopwatch/StopWatchAct.java
+++ app/src/main/java/com/example/stopwatch/StopWatchAct.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 
 public class StopWatchAct extends AppCompatActivity implements View.OnClickListener {
@@ -15,6 +17,7 @@ public class StopWatchAct extends AppCompatActivity implements View.OnClickListe
     Button btnStart, btnStop;
     ImageView icanchor;
     Animation roundingAlone;
+    Chronometer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,7 @@ public class StopWatchAct extends AppCompatActivity implements View.OnClickListe
         btnStart = findViewById(R.id.btnStart);
         btnStop = findViewById(R.id.btnStop);
         icanchor = findViewById(R.id.ivCircle);
+        timer = findViewById(R.id.timer);
 
         // animation
         roundingAlone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
@@ -50,9 +54,27 @@ public class StopWatchAct extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v)
     {
-        icanchor.startAnimation(roundingAlone);
-        btnStop.animate().alpha(1).translationY(-80).setDuration(300).start();
-        btnStart.animate().alpha(0).setDuration(300).start();
+
+
+        switch (v.getId())
+        {
+            case R.id.btnStart:
+                icanchor.startAnimation(roundingAlone);
+                btnStop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnStart.animate().alpha(0).setDuration(300).start();
+
+                // Start Time
+                timer.setBase(SystemClock.elapsedRealtime());
+                timer.start();
+                break;
+            case R.id.btnStop:
+
+                icanchor.clearAnimation();
+                btnStart.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnStop.animate().alpha(0).setDuration(300).start();
+                timer.stop();
+                break;
+        }
 
     }
 }
