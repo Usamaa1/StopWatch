bd936faa4fda135f25c047602454fcea86efe1c8
Haider <shkhaider2015@gmail.com>
2020-01-09 10:04:29 +0500

    adding animation

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash, tvSubSplash;
+    Button btnSplash;
+    Animation atg;
+    ImageView ivSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init()
+    {
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnSplash = findViewById(R.id.btnSplash);
+        ivSplash = findViewById(R.id.ivSplash);
+
+        // load animation
+        atg = AnimationUtils.loadAnimation(this, R.anim.atg);
+
+        //passing animation
+        ivSplash.startAnimation(atg);
+
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        btnSplash.setTypeface(MLight);
+    }
+
+
 }
